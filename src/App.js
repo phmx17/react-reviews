34b682986a61f1c6742961530a303fe01f95670d
Slim Yelow <slimyelow@gmail.com>
2021-06-17 20:09:04 +0200

    add title page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Review from './Review';
+import { AiFillGithub } from 'react-icons/ai'
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <main>
+    <section className="container">
+      <div className="title">
+        <h2>reviews project setup</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
+    <AiFillGithub className="icon" />
+    </main>
 }
 
 export default App;
